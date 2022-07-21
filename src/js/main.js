009b4ee0cf28e6efedee8b2132be960abdbed0de
Andrey <andrejchubov@yandex.ru>
2022-07-21 17:23:21 +0300

    продожаем настройку приложения

---
+++ src/js/main.js
@@ -0,0 +1,24 @@
+let btnStart = document.getElementById('start'),
+    budgetValue = document.etElementsByClassName('budget-value'),
+    daybudgetValue = document.getElementsByClassName('daybudget-value'),
+    levelValue = document.getElementsByClassName('level-value'),
+    expensesValue = document.getElementsByClassName('expenses-value'),
+    optionalexpensesValue = document.getElementsByClassName('optionalexpenses-value'),
+    incomeValue = document.getElementsByClassName('income-value'),
+    monthsavingsValue = document.getElementsByClassName('monthsavings-value'),
+    yearsavingsValue = document.getElementsByClassName('yearsavings-value'),
+    inpExpenses = document.getElementsByClassName('expenses-item'),
+    btnRaschet = document.getElementsByClassName('count-budget-btn'),
+    btnUtver = document.getElementsByClassName('optionalexpenses-btn'),
+    btnUtver1= document.getElementsByClassName('expenses-item-btn'),
+    inpOptExpen = document.querySelectorAll('.optionalexpenses-item'),
+    inpChooseInc = document.querySelector('.choose-income'),
+    inpChkSav = document.getElementById('savings'),
+    inpSum = document.getElementById('sum'),
+    inpPercent = document.getElementById('percent'),
+    inpYear = document.querySelector('.year-value'),
+    inpMonth = document.querySelector('."month-value'),
+    inpDay = document.querySelector('.day-value');
+
+
+
